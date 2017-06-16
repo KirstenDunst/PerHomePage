7eaf326f94f78671bbbbfde9035704187a440519
KirstenDunst <cao_shixin@yahoo.com>
2017-06-16 20:03:57 +0800

    点击按钮跳出frame框架使整个界面跳出进入新的页面

--- js/granduateStudent1.js
+++ js/granduateStudent1.js
@@ -11,7 +11,6 @@ function changeFrameSrc (id,targetSrc) {
 function getRandomColorForBackground () {
 	//生成随机数
 //	var a = Math.floor(Math.random()*255+1);
-	
 	var c = '#'; 
     var cArray = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']; 
     for(var i = 0; i < 6;i++) 
@@ -20,4 +19,18 @@ function getRandomColorForBackground () {
        c += cArray[cIndex]; 
      } 
 	document.body.style.backgroundColor = c;
+}
+
+//让iframe里面的按钮点击整个界面换新的。
+function reloadANew () {
+	alert("跳出");
+//	top.location = "http://www.baidu.com"  
+	self.location.href = "http://www.baidu.com/";
+	
+	/*
+	 url直接跳转
+     js中的window.location.href
+     window.navigate()
+     self.location.href
+	 */
 }
